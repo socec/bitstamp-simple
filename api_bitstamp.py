f801b67649dc8f5cd095a6fa045d1fa5173002bd
Igor Socec <igorsocec@gmail.com>
2014-09-20 16:28:21 +0200

    adding files

---
+++ api_bitstamp.py
@@ -0,0 +1,141 @@
+# Bitstamp HTTP API functions
+# https://www.bitstamp.net/api/
+# =============================
+
+import hmac, hashlib
+import time
+import urllib2, json
+
+# authorization functions
+# =======================
+
+def update_nonce(nonce):
+	# return greater value between incremented nonce and current timestamp
+	return int(max(int(nonce)+1, int(time.time())))
+
+def get_authorization(api_key, api_secret, client_id, nonce):
+	nonce = str(nonce)
+	message = nonce + client_id + api_key
+	signature = hmac.new(api_secret, msg=message, digestmod=hashlib.sha256).hexdigest().upper()
+	ksn = "key=" + str(api_key) + "&signature=" + str(signature) + "&nonce=" + nonce
+	return ksn
+
+# API functions
+# =============
+
+base_url = "https://www.bitstamp.net/api"
+
+def _api_http_communication(url, data):
+	connection = urllib2.urlopen(url, data)
+	response = connection.read()
+	connection.close()
+	return response
+
+def api_balance(authorization):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+
+	# Returns JSON dictionary:
+	# usd_balance - USD balance
+	# btc_balance - BTC balance
+	# usd_reserved - USD reserved in open orders
+	# btc_reserved - BTC reserved in open orders
+	# usd_available- USD available for trading
+	# btc_available - BTC available for trading
+	# fee - customer trading fee
+
+	url = base_url + "/balance/"
+	data = authorization
+	return _api_http_communication(url, data)
+
+def api_user_transactions(authorization, offset = 0, limit = 200, sort = "desc"):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+	# offset - skip that many transactions before beginning to return results. Default: 0.
+	# limit - limit result to that many transactions. Default: 100.
+	# sort - sorting by date and time (asc - ascending; desc - descending). Default: desc.
+
+	# Returns descending JSON list of transactions. Every transaction (dictionary) contains:
+	# datetime - date and time
+	# id - transaction id
+	# type - transaction type (0 - deposit; 1 - withdrawal; 2 - market trade)
+	# usd - USD amount
+	# btc - BTC amount
+	# fee - transaction fee
+	# order_id - executed order id
+
+	url = base_url + "/user_transactions/"
+	data = authorization + "&offset=" + str(offset) + "&limit=" + str(limit) + "&sort=" + sort
+	return _api_http_communication(url, data)
+
+def api_open_orders(authorization):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+
+	# Returns JSON list of open orders. Each order is represented as dictionary:
+	# id - order id
+	# datetime - date and time
+	# type - buy or sell (0 - buy; 1 - sell)
+	# price - price
+	# amount - amount
+
+	url = base_url + "/open_orders/"
+	data = authorization
+	return _api_http_communication(url, data)
+
+def api_cancel_order(authorization, order_id):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+	# id - order ID
+
+	# Returns 'true' if order has been found and canceled.
+
+	url = base_url + "/cancel_order/"
+	data = authorization + "&id=" + str(order_id)
+	return _api_http_communication(url, data)
+
+def api_buy(authorization, amount, price):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+	# amount - amount
+	# price - price
+
+	# Returns JSON dictionary representing order:
+	# id - order id
+	# datetime - date and time
+	# type - buy or sell (0 - buy; 1 - sell)
+	# price - price
+	# amount - amount
+
+	url = base_url + "/buy/"
+	data = authorization + "&amount=" + ("%.8f" % amount) + "&price=" + str(price)
+	return _api_http_communication(url, data)
+
+def sell(authorization, amount, price):
+	# Params:
+	# key - API key
+	# signature - signature
+	# nonce - nonce
+	# amount - amount
+	# price - price
+
+	# Returns JSON dictionary representing order:
+	# id - order id
+	# datetime - date and time
+	# type - buy or sell (0 - buy; 1 - sell)
+	# price - price
+	# amount - amount
+
+	url = base_url + "/sell/"
+	data = authorization + "&amount=" + ("%.8f" % amount) + "&price=" + str(price)
+	return _api_http_communication(url, data)
---
+++ bitstamp-simple.py
@@ -0,0 +1,229 @@
+import cmd, json
+import api_bitstamp as bs
+
+# Command interpreter for Bitstamp API
+class BitstampCmd(cmd.Cmd):
+
+	# session data
+	# ============
+	nonce = 0
+	api_key = "xxx"
+	api_secret = "xxx"
+	client_id = "xxx"
+
+	# overriding interpreter setup
+	# ============================
+	prompt = "bstamp$ "
+
+	def emptyline(self):
+		return
+
+	def default(self, args):
+		print "*** unknown command ***"
+		self.do_help(None)
+
+	def preloop(self):
+		auth_data = self._auth_load()
+		if (auth_data != []):
+			self.api_key, self.api_secret, self.client_id = auth_data
+		return
+
+	def postloop(self):
+		self.api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+		self.api_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+		self.client_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+	def precmd(self, args):
+		arglist = self._args_to_list(args)
+		if (len(arglist) != 0) and (arglist[0] == "authorization"):
+			return args
+		if (self.api_key == "xxx") or (self.api_secret == "xxx") or (self.client_id == "xxx"):
+			print "No authorization data, please run authorization command."
+		return args
+
+	# private functions
+	# =================
+
+	# prepare API authorization
+	def _api_auth(self):
+		self.nonce = bs.update_nonce(self.nonce)
+		return bs.get_authorization(self.api_key, self.api_secret, self.client_id, self.nonce)
+
+	# convert a string with arguments to a list with arguments
+	def _args_to_list(self, args):
+		if (len(args) == 0):
+			return []
+		else:
+			return args.split()
+
+	# check if share range is invalid
+	def _share_invalid(self, share):
+		if (float(share) < 0.0) or (float(share) > 1.0):
+			print "Requested share {} is not in valid range [0.0 - 1.0].".format(share)
+			return True
+		else:
+			return False
+
+	# store authorization data
+	def _auth_save(self, api_key, api_secret, client_id):
+		with open('authpyc', 'w') as f:
+			f.write(api_key + api_secret + client_id)
+
+	# load authorization data
+	def _auth_load(self):
+		try:
+			with open('authpyc', 'r') as f:
+				auth = f.read()
+		except:
+			print "Can't open authorization data file."
+			return []
+		if (len(auth) != 70):
+			print "Authorization data file is corrupted ({}). ".format(len(auth))
+			return []
+		api_key = auth[0:32]
+		api_secret = auth[32:64]
+		client_id = auth[64:70]
+		return api_key, api_secret, client_id
+
+	# commands
+	# ========
+
+	# enter authorization data
+	def help_authorization(self):
+		print "USAGE:"
+		print "\t authorization <api_key> <api_secret> <client_id> [-s]"
+		print "DESCRIPTION:"
+		print "\t Enter API key, API secret and client ID for authorization of private API calls."
+		print "\t Providing \"-s\" option stores them (plaintext) to a file in the current directory."
+	def do_authorization(self, args):
+		arglist = self._args_to_list(args)
+		if ((len(arglist) < 3) or (len(arglist) > 4)):
+			self.do_help("authorization")
+			return
+		self.api_key = arglist[0]
+		self.api_secret = arglist[1]
+		self.client_id = arglist[2]
+		if ((len(arglist) == 4) and (arglist[3] == "-s")):
+			self._auth_save(self.api_key, self.api_secret, self.client_id)
+
+	# show balance
+	def help_balance(self):
+		print "USAGE:"
+		print "\t balance"
+		print "DESCRIPTION:"
+		print "\t Show current account balance."
+	def do_balance(self, args):
+		auth = self._api_auth()
+		ret = json.loads(bs.api_balance(auth))
+		print json.dumps(ret, indent=2)
+
+	# buy BTC
+	def help_buy(self):
+		print "USAGE:"
+		print "\t buy <usd_share> <price>"
+		print "DESCRIPTION:"
+		print "\t Buy BTC at requested price by spending requested share [0.0, 1.0] of available USD."
+		print "\t e.g. buy 0.25 100.0"
+	def do_buy(self, args):
+		arglist = self._args_to_list(args)
+		if (len(arglist) != 2):
+			self.do_help("buy")
+			return
+		usd_share, price = arglist
+		if (self._share_invalid(usd_share)):
+			return
+		auth = self._api_auth()
+		usd_available = json.loads(bs.api_balance(auth))["usd_available"]
+		btc_amount = (float(usd_share) * float(usd_available)) / float(price)
+		if (btc_amount == 0.0):
+			print "BTC amount is 0"
+			return
+		print "buying {} BTC for {} USD/BTC".format(btc_amount, price)
+		auth = self._api_auth()
+		print bs.api_buy(auth, btc_amount, price)
+
+	# sell BTC
+	def help_sell(self):
+		print "USAGE:"
+		print "\t sell <btc_share> <price>"
+		print "DESCRIPTION:"
+		print "\t Sell requested share [0.0, 1.0] of available BTC at requested price."
+		print "\t e.g. sell 0.25 100.0"
+	def do_sell(self, args):
+		arglist = self._args_to_list(args)
+		if (len(arglist) != 2):
+			self.do_help("sell")
+			return
+		btc_share, price = arglist
+		if (self._share_invalid(btc_share)):
+			return
+		auth = self._api_auth()
+		btc_available = json.loads(bs.api_balance(auth))["btc_available"]
+		btc_amount = float(btc_share) * float(btc_available)
+		if (btc_amount == 0.0):
+			print "BTC amount is 0"
+			return
+		print "selling {} BTC for {} USD/BTC".format(btc_amount, price)
+		auth = self._api_auth()
+		print bs.api_sell(auth, btc_amount, price)
+
+	# show orders
+	def help_orders(self):
+		print "USAGE:"
+		print "\t orders"
+		print "DESCRIPTION:"
+		print "\t Show currently open orders."
+	def do_orders(self, args):
+		auth = self._api_auth()
+		ret = json.loads(bs.api_open_orders(auth))
+		for item in ret:
+			print item
+
+	# cancel order
+	def help_cancel(self):
+		print "USAGE:"
+		print "\t cancel <order_id>"
+		print "DESCRIPTION:"
+		print "\t Cancel an open order."
+	def do_cancel(self, args):
+		arglist = self._args_to_list(args)
+		if (len(arglist) != 1):
+			self.do_help("cancel")
+			return
+		order_id = arglist[0]
+		auth = self._api_auth()
+		print bs.api_cancel_order(auth, order_id)
+
+	# show transactions
+	def help_transactions(self):
+		print "USAGE:"
+		print "\t transactions [num_last]"
+		print "DESCRIPTION:"
+		print "\t Show list of transactions in descending order."
+		print "\t Providing [num_last] parameter will show only that many last transactions."
+	def do_transactions(self, args):
+		arglist = self._args_to_list(args)
+		if (len(arglist) > 1):
+			self.do_help("transactions")
+			return
+		auth = self._api_auth()
+		if (len(arglist) == 1):
+			limit = arglist[0]
+			ret = json.loads(bs.api_user_transactions(auth, limit=limit))
+		else:
+			ret = json.loads(bs.api_user_transactions(auth))
+		for item in ret:
+			print item
+
+	# exit
+	def help_exit(self):
+		print "USAGE:"
+		print "\t exit"
+		print "DESCRIPTION:"
+		print "\t Exit the program."
+	def do_exit(self, args):
+		return True
+
+# execute if module is run directly (not imported)
+if __name__ == "__main__":
+	BitstampCmd().cmdloop()
