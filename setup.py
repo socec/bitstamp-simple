683f3ecff21acb2c3cb64d4808c786112cc3bc9e
Igor Socec <igorsocec@gmail.com>
2017-07-01 15:53:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='bitstamp-simple',
+    version='1.0',
+    description='A simple Bitstamp API client',
+    long_description=readme,
+    url='https://github.com/socec/bitstamp-simple',
+    author='Igor Socec',
+    author_email='igorsocec@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['pycrypto'],
+    entry_points={
+            'console_scripts': [
+                'sample=sample:main',
+            ],
+        },
+)
